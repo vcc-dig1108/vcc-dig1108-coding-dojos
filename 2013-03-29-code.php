7bc310ac9158f1f6575fb56411dfc7015096f33c
David Rogers <al.the.x@gmail.com>
2013-03-29 11:21:32 -0400

    @demet19 @kilofoxx refactored addition()

--- 2013-03-29-code.php
+++ 2013-03-29-code.php
@@ -5,26 +5,19 @@
  *   - Commit between pairs!
  */
 
-$data = array(
-    "1" => array(+1, +1, +2),
-    "2" => array(+1, -1, +0),
-    "3" => array(+1, +0, +1),
-    "4" => array(+0, +1, +1),
-    "5" => array(+0, -1, -1),
-    "6" => array(+0, +0, +0),
-    "7" => array(-1, +1, +0),
-    "8" => array(-1, -1, -2),
-    "9" => array(-1, +0, -1),
-);
-
-foreach($data as $key => $value)
+function test($name, $data)
 {
-    list($a, $b, $c) = $value;
+    foreach($data as $key => $value)
+    {
+        list($a, $b, $c) = $value;
 
-    echo "{$data}[$key] => ${a}, {$b}, {$c} \n";
+        assert("{$name}({$a}, {$b}) == {$c}");
+
+        echo "{$name}[$key] => {$a}, {$b}, {$c} \n";
+    }
 }
 
-function add($a, $b)
+function addition($a, $b)
 {
     return $a + $b;
 }
