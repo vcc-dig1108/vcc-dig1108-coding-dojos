5f023b3a4864c1d2be08d6cce64592082cb1352d
David Rogers <al.the.x@gmail.com>
2013-04-05 11:33:02 -0400

    Setting up a little more, for convenience.

---
+++ 2013-04-05-code.php
@@ -0,0 +1,12 @@
+<?php // PROD CODE
+/**
+ * A hallway has 100 closed doors.
+ * Take a walk down the hallway. Stop at every door and open it.
+ * Take a walk down the hallway. Stop at every _other_ door and close it.
+ * Take a walk down the hallway. Stop at every _third_ door.
+ *      If open, close it. If closed, open it.
+ * Take a walk down the hallway. Stop at every _fourth_ door.
+ *      If open, close it. If closed, open it.
+ * Repeat this pattern 100 times.
+ * What is the state of the hallway?
+ */
---
+++ 2013-04-05-tests.php
@@ -0,0 +1,7 @@
+<?php // TEST CODE
+
+require '2013-04-05-code.php';
+
+// Tests go here...
+
+echo 'Ran tests in ' . basename(__FILE__);
